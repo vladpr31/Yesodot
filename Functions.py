13c53e89ea0ba0e0f4880af9bca8ce162ad9c257
Alex Tatievsky <alextatie@gmail.com>
2019-12-08 17:14:39 +0200

    signin, login

--- Functions.py
+++ Functions.py
@@ -27,45 +27,86 @@ def add_exam():
     newExam = Exam(semester, solFile, examFile, fformat, subName, solType)
     return newExam
 
+
 def Signup():
+    def usercheck():
+        i = 0
+        while i == 0:
+            user = input("Username:\n\r")
+            with open("LoginCred.txt") as g:
+                if "Username: "+user+" " in g.read():
+                    print("Username taken, try again.\n\r")
+                else:
+                    i = 1
+                    return user
     f = open("LoginCred.txt", "a")
     option = input("Choose:\n\r(1)Student (2)Teacher (3)Coordinator\n\r")
     if option == '1':
         name = input("Name:\n\r")
-        f.write("Student_Name: " + name + "\n\r")
-        username = input("Username:\n\r")
-        f.write("Username: " + username + "\n\r")
+        f.write("Student_Name: " + name + " ")
+        username = usercheck()
+        f.write("Clearance: 1" + " ")
+        f.write("Username: " + username + " ")
         password = input("Password:\n\r")
-        f.write("Password: " + password + "\n\r")
+        f.write("Password: " + password + " ")
         year = input("Year:\n\r")
-        f.write("Year: " + year + "\n\n\r")
+        f.write("Year: " + year + "\n\r")
         newStudent = Student(name, username, password, "Student", year)
         f.close()
         return newStudent
     elif option == '2':
         f = open("LoginCred.txt", "a")
         name = input("Name:\n\r")
-        f.write("Tutor_Name: " + name + "\n\r")
+        f.write("Tutor_Name: " + name + " ")
         username = input("Username:\n\r")
-        f.write("Username: " + username + "\n\r")
+        f.write("Clearance: 2" + " ")
+        f.write("Username: " + username + " ")
         password = input("Password:\n\r")
-        f.write("Password: " + password + "\n\r")
+        f.write("Password: " + password + " ")
         subject = input("Subject:\n\r")
-        f.write("Teaching Subject: " + subject + "\n\n\r")
+        f.write("Teaching Subject: " + subject + "\n\r")
         newTeacher = Staff(name, username, password, 2, subject)
         f.close()
         return newTeacher
     elif option == '3':
         f = open("LoginCred.txt", "a")
         name = input("Name:\n\r")
-        f.write("Tutor_Name: " + name + "\n\r")
+        f.write("Tutor_Name: " + name + " ")
+        f.write("Clearance: 3" + " ")
         username = input("Username:\n\r")
-        f.write("Username: " + username + "\n\r")
+        f.write("Username: " + username + " ")
         password = input("Password:\n\r")
-        f.write("Password: " + password + "\n\r")
+        f.write("Password: " + password + " ")
         subject = input("Subject:\n\r")
-        f.write("Teaching Subject: " + subject + "\n\n\r")
+        f.write("Teaching Subject: " + subject + "\n\r")
         newCoordinator = Staff(name, username, password, 3, subject)
         f.close()
         return newCoordinator
 
+
+def Login():
+    i = 0
+    user = 0
+    while i == 0:
+        user = input("Username:\n\r")
+        with open("LoginCred.txt") as g:
+            if "Username: "+user not in g.read():
+                print("Wrong username, try again.\n\r")
+            else:
+                i = 1
+    i = 0
+    while i == 0:
+        password = input("Password:\n\r")
+        with open("LoginCred.txt") as g:
+            if "Username: "+user+" Password: "+password+" " not in g.read():
+                print("Wrong password, try again.\n\r")
+            else:
+                i = 1
+    print("Welcome "+user+"!\n\r")
+    with open("LoginCred.txt") as g:
+        if "Clearance: 1 Username: " + user + " " in g.read():
+            return 1
+        elif "Clearance: 2 Username: " + user + " " in g.read():
+            return 2
+        else:
+            return 3
--- Main.py
+++ Main.py
@@ -1,5 +1,19 @@
 from Functions import Signup
+from Functions import Login
 
 
 PersonDataBase = []
-PersonDataBase.append(Signup())
+ExamDataBase = []
+QuestionDataBase = []
+Myclearance = 0
+option = input("(1) Log in\n\r(2) Sign up\n\r")
+if option == '1':
+    Myclearance=Login()
+else:
+    PersonDataBase.append(Signup())
+    print("Please log in:\n\r")
+    Myclearance = Login()
+if Myclearance == 1 or Myclearance == 2:
+    Text = input("Search:\n\r")
+else:
+    option = input("(1) Search\n\r(2) add exam\n\r")
