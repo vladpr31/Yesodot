ac0d295a09eea65dc270718822a71f9ada022762
Vlad Propisnov <vladip31@gmail.com>
2019-12-08 15:09:13 +0200

    Added few stuff

--- TempMain.py
+++ TempMain.py
@@ -23,29 +23,41 @@ def Signup():
     option = input("Choose:\n\r(1)Student (2)Teacher (3)Coordinator\n\r")
     if option == '1':
         name = input("Name:\n\r")
-        f.write("Student_Name: "+name+"\n\r")
+        f.write("Student_Name: " + name + "\n\r")
         username = input("Username:\n\r")
-        f.write("Username: "+username+"\n\r")
+        f.write("Username: " + username + "\n\r")
         password = input("Password:\n\r")
-        f.write("Password: "+password+"\n\r")
+        f.write("Password: " + password + "\n\r")
         year = input("Year:\n\r")
-        f.write("Year: "+year+"\n\n\r")
+        f.write("Year: " + year + "\n\n\r")
         newStudent = Student(name, username, password, "Student", year)
         f.close()
         return newStudent
     elif option == '2':
+        f = open("LoginCred.txt", "a")
         name = input("Name:\n\r")
+        f.write("Tutor_Name: " + name + "\n\r")
         username = input("Username:\n\r")
+        f.write("Username: " + username + "\n\r")
         password = input("Password:\n\r")
+        f.write("Password: " + password + "\n\r")
         subject = input("Subject:\n\r")
+        f.write("Teaching Subject: " + subject + "\n\n\r")
         newTeacher = Staff(name, username, password, 2, subject)
+        f.close()
         return newTeacher
     elif option == '3':
+        f = open("LoginCred.txt", "a")
         name = input("Name:\n\r")
+        f.write("Tutor_Name: " + name + "\n\r")
         username = input("Username:\n\r")
+        f.write("Username: " + username + "\n\r")
         password = input("Password:\n\r")
+        f.write("Password: " + password + "\n\r")
         subject = input("Subject:\n\r")
+        f.write("Teaching Subject: " + subject + "\n\n\r")
         newCoordinator = Staff(name, username, password, 3, subject)
+        f.close()
         return newCoordinator
 
 
