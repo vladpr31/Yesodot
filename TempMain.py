97a4515095d12fd2fd8a4b4eb28ed956af63dcab
Vlad Propisnov <vladip31@gmail.com>
2019-12-08 14:47:39 +0200

    Added demolist

--- TempMain.py
+++ TempMain.py
@@ -19,13 +19,19 @@ class Staff(Person):
 
 
 def Signup():
+    f = open("LoginCred.txt", "a")
     option = input("Choose:\n\r(1)Student (2)Teacher (3)Coordinator\n\r")
     if option == '1':
         name = input("Name:\n\r")
+        f.write("Student_Name: "+name+"\n\r")
         username = input("Username:\n\r")
+        f.write("Username: "+username+"\n\r")
         password = input("Password:\n\r")
+        f.write("Password: "+password+"\n\r")
         year = input("Year:\n\r")
-        newStudent = Student(name, username, password, 1, year)
+        f.write("Year: "+year+"\n\n\r")
+        newStudent = Student(name, username, password, "Student", year)
+        f.close()
         return newStudent
     elif option == '2':
         name = input("Name:\n\r")
@@ -42,8 +48,8 @@ def Signup():
         newCoordinator = Staff(name, username, password, 3, subject)
         return newCoordinator
 
+
 # main
 PersonDataBase = []
 PersonDataBase.append(Signup())
-PersonDataBase.append(Signup())
-print(PersonDataBase[0].name, PersonDataBase[1].name)
+print(PersonDataBase[0].name)
